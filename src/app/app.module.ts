9df30bf24a7f3b3f07ee12ff8d86caecf2b5aa65
chaitanyachekuri <cskcvarma16@icloud.com>
2017-05-05 16:48:39 -0400

    second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+// import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
+// const appRoutes: Routes = [
+//   {}
+// ]
+// RouterModule.forRoot(appRoutes)
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
